345ab35785472cffdef9e719e05b0fc46e87777a
calebm01 <calebrmouritsen@gmail.com>
2024-12-10 14:16:02 -0500

    social site implemented

--- mysite/socialsite/admin.py
+++ mysite/socialsite/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from .models import Profile, Post
+
+
 
 # Register your models here.
+
+admin.site.unregister(User)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+
+    model = User
+
+    # Only display the "username" field
+
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+admin.site.register(User, UserAdmin) 
+
+# admin.site.register(Profile)
+
+admin.site.unregister(Group)
+
+admin.site.register(Post)
--- mysite/socialsite/views.py
+++ mysite/socialsite/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
+from .forms import PostForm
+from .models import Profile
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+
+from django.shortcuts import render
+
+def dashboard(request):
+
+    form = PostForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+
+            post = form.save(commit=False)
+
+            post.user = request.user
+
+            post.save()
+            return redirect("socialsite:dashboard")
+        
+    return render(request, "socialsite/dashboard.html", {"form": form})
+
+def profile_list(request):
+
+    profiles = Profile.objects.exclude(user=request.user)
+
+    return render(request, "socialsite/profile_list.html", {"profiles": profiles})
+
+def profile(request, pk):
+
+    if not hasattr(request.user, 'profile'):
+        missing_profile = Profile(user=request.user)
+        missing_profile.save()
+
+    profile = Profile.objects.get(pk=pk)
+    if request.method == "POST":
+
+        current_user_profile = request.user.profile
+
+        data = request.POST
+
+        action = data.get("follow")
+
+        if action == "follow":
+
+            current_user_profile.follows.add(profile)
+
+        elif action == "unfollow":
+
+            current_user_profile.follows.remove(profile)
+
+        current_user_profile.save()
+    return render(request, "socialsite/profile.html", {"profile": profile})
